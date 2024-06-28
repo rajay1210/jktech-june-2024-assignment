69336a0708bac134d402f5a813b2b12b4b3a0bef
rajay1210 <ajjukumar.ar@gmail.com>
2024-06-28 12:12:09 +0530

    gunicorn worker changes

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ backlog = 2048
 #       A positive integer. Generally set in the 1-5 seconds range.
 #
 
-workers = 1
+workers = 2
 worker_class = 'uvicorn.workers.UvicornWorker'
 worker_connections = 1000
 timeout = 30
