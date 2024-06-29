b2ba4ff2fcb5bcf4ea8f3ebaff57e72e96511849
rajay1210 <ajjukumar.ar@gmail.com>
2024-06-29 15:30:45 +0530

    change in worker count

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ backlog = 2048
 #       A positive integer. Generally set in the 1-5 seconds range.
 #
 
-workers = 10
+workers = 8
 worker_class = 'uvicorn.workers.UvicornWorker'
 worker_connections = 1000
 timeout = 30
